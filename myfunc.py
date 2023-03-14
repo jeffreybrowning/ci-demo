d7f294533fa5021374095cf150c8608cec56f895
Jeff Browning <jeff@thebeans.io>
2023-03-13 22:50:22 -0400

    Update myfunc.py

--- myfunc.py
+++ myfunc.py
@@ -1,3 +1,4 @@
+from time import sleep
 
 def my_func(context, p1=1, p2='a-string'):
     # access input metadata, values, files, and secrets (passwords)
@@ -11,4 +12,5 @@ def my_func(context, p1=1, p2='a-string'):
     context.log_result('accuracy', p1 * 2)
     context.log_result('loss', p1 * 3)
     context.set_label('framework', 'sklearn')
-
+    while True:
+        sleep(100)
